904d363186d57c5f4af68c299ad3e4830e7f6c83
Dheeraj Raina <dheerajraina55@gmail.com>
2024-04-29 16:09:48 +0530

    Bug fixed in self attention block

--- self_attention.py
+++ self_attention.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 import keras
 
 
-class SelfAttention(tf.Module):# Scaled Dot Product Attention
-    def __init__(self, embed_size, heads) -> None:
+class SelfAttention(tf.Module):  # Scaled Dot Product Attention
+    def __init__(self, embed_size, heads):
         super(SelfAttention, self).__init__()
         self.embed_size = embed_size
         self.heads = heads
@@ -17,24 +17,30 @@ class SelfAttention(tf.Module):# Scaled Dot Product Attention
             self.head_dim, input_shape=(self.head_dim,), use_bias=False)
         self.queries = keras.layers.Dense(
             self.head_dim, input_shape=(self.head_dim,), use_bias=False)
-        self.fc_out = keras.layers.Dense(embed_size,input_shape=(heads*self.head_dim,))
+        self.fc_out = keras.layers.Dense(
+            embed_size, input_shape=(heads*self.head_dim,))
 
-    def call(self, values, keys, query, mask):
+    def __call__(self, values, keys, query, mask):
         N = query.shape[0]
         value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
 
-        values = values.reshape(N, value_len, self.heads, self.head_dim)
-        keys = values.reshape(N, key_len, self.heads, self.head_dim)
-        queries = query.reshape(N, query_len, self.heads, self.head_dim)
+        values = tf.reshape(values, (N, value_len, self.heads, self.head_dim))
+        keys = tf.reshape(values, (N, key_len, self.heads, self.head_dim))
+        queries = tf.reshape(query, (N, query_len, self.heads, self.head_dim))
+
+        values = self.values(values)
+        keys = self.keys(keys)
+        queries = self.queries(queries)
 
         energy = tf.einsum("nqhd,nkhd->nhqk", queries, keys)
 
         if mask is not None:
-            energy = tf.where(0, float("-1e20"), energy)
+            mask = tf.equal(energy, 0)
+            energy = tf.where(mask, float("-1e20"), energy)
         attention = tf.nn.softmax(energy/(self.embed_size**(1/2)), axis=3)
 
-        out = tf.einsum("nhql,nlhd->nqhd", attention,
-                        values).reshape(N, query_len, self.heads*self.head_dim)
+        out = tf.reshape(tf.einsum("nhql,nlhd->nqhd", attention,
+                                   values), (N, query_len, self.heads*self.head_dim))
 
         out = self.fc_out(out)
         return out
